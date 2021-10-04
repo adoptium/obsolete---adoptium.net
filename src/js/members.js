23dacd537ca3a9d66c1d7df9a68b3f07ae86473a
George Adams <georgeadams1995@gmail.com>
2021-10-04 17:17:40 +0100

    convert members list to json and add sponsors page (#191)
    * convert members list to json and add sponsors page
    
    * add sponsorship agreement

---
+++ src/js/members.js
@@ -0,0 +1,49 @@
+const {getMembers} = require('./common');
+
+let strategicMembers = [];
+let enterpriseMembers = [];
+let participantMembers = [];
+
+module.exports.load = () => {
+  let members = getMembers()
+  for (let member of members) {
+    switch(member.tier) {
+      case 'strategic':
+        strategicMembers.push(member)
+      break;
+      case 'enterprise':
+        enterpriseMembers.push(member)
+      break;
+      case 'participant':
+        participantMembers.push(member)
+      break;
+    }
+  }
+
+  // Randomly mix up members logos
+  shuffle(strategicMembers);
+  shuffle(enterpriseMembers);
+  shuffle(participantMembers);
+
+  const templateSelector = Handlebars.compile(document.getElementById('template-selector').innerHTML);
+  document.getElementById('members').innerHTML = templateSelector({strategicMembers, enterpriseMembers, participantMembers});
+}
+
+// Randomly shuffle members
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
---
+++ src/js/sponsors.js
@@ -0,0 +1,39 @@
+const {getMembers} = require('./common');
+
+let sponsors = [];
+
+module.exports.load = () => {
+  let members = getMembers()
+  for (let member of members) {
+    switch(member.tier) {
+      case 'sponsor':
+        sponsors.push(member)
+      break;
+    }
+  }
+
+  // Randomly mix up members logos
+  shuffle(sponsors);
+
+  const templateSelector = Handlebars.compile(document.getElementById('template-selector').innerHTML);
+  document.getElementById('members').innerHTML = templateSelector({sponsors});
+}
+
+// Randomly shuffle members
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
